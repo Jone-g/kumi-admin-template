79bc4a9ac88d3dcb25892ddc67315b3a0d155dee
Jone <gaoguohui@yunwii.com>
2017-11-13 19:38:09 +0800

    更改element默认样式

--- src/views/layout/Navbar.vue
+++ src/views/layout/Navbar.vue
@@ -58,6 +58,9 @@ export default {
             height: 50px;
             float: left;
             padding: 0 10px;
+            &:focus {
+              outline: none;
+            }
         }
         .errLog-container {
             display: inline-block;
